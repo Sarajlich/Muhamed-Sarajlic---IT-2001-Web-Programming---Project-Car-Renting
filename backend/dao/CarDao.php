47138e264974641a786ebb1e032770f1838e6b90
RUN <sarajlich.19@gmail.com>
2025-11-11 16:39:53 +0100

    .

--- backend/dao/CarDao.php
+++ backend/dao/CarDao.php
@@ -6,46 +6,10 @@ class CarDao extends BaseDao{
         parent::__construct('cars');
     }
 
-    public function createCar($car){
-        $data = [
-            'brand'        => $car['brand'],
-            'model'        => $car['model'],
-            'year'         => $car['year'],
-            'price_per_day'=> $car['price_per_day'],
-            'status'    => $car['status'],
-            'category_id'  => $car['category_id']
-        ];
-        return $this->insert($data);
-    }
-
-    public function getAllCars(){
-        return $this->getAll();
-    }
-
-    public function getCarById($id){
-        return $this->getById($id);
-    }
-
     public function getByBrand($brand) {
         $stmt = $this->connection->prepare("SELECT * FROM cars WHERE brand = :brand");
         $stmt->execute(['brand' => $brand]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function updateCar($id, $car){
-        $data = [
-            'brand'        => $car['brand'],
-            'model'        => $car['model'],
-            'year'         => $car['year'],
-            'price_per_day'=> $car['price_per_day'],
-            'status'    => $car['status'],
-            'category_id'  => $car['category_id']
-        ];
-        return $this->update($id, $data);
-    }
-
-    public function deleteCar($id){
-        return $this->delete($id);
     }
 }
 ?>
